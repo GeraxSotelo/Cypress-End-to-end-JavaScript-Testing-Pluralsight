a998f1eeb4eb99052f271408c5056b108fa7f7da
Gerax Sotelo <gsotelo23@gmail.com>
2020-05-19 16:17:40 -0600

    Write first test in my_first_spec.js

--- cypress/integration/my_first_spec.js
+++ cypress/integration/my_first_spec.js
@@ -1,6 +1,13 @@
 /// <reference types="Cypress" />
 
-
+//The describe function is used to describe the test suite by giving it a unique name
+describe("My First Test Suite", () => {
+    //The 'it' function comes from Mocha. Gives a name to the test
+    it('test url works', () => {
+        //All cypress commands begin with cy
+        cy.visit('http://localhost:4100');
+    });
+})
 
 
 
