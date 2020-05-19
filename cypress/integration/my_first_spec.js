6ef84ba0615dfb537b2945e597c2e8c88f3de1c7
Gerax Sotelo <gsotelo23@gmail.com>
2020-05-19 16:36:43 -0600

    Write test using cy.contains

--- cypress/integration/my_first_spec.js
+++ cypress/integration/my_first_spec.js
@@ -7,6 +7,12 @@ describe("My First Test Suite", () => {
         //All cypress commands begin with cy
         cy.visit('http://localhost:4100');
     });
+    it('test signup exists', () => {
+        //Use the cy.contains() function to get the DOM element containing certain text
+        //Verify the DOM element 'a' with the class 'nav-link' exists and contains the text 'Sign-up'
+        //Use the click() function to click on an element that Cypress found
+        cy.contains('a.nav-link', 'Sign up').click();
+    });
 })
 
 
