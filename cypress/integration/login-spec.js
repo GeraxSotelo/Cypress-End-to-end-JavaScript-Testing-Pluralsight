18a68c912008135949c50840761b4d77dc129323
Gerax Sotelo <gsotelo23@gmail.com>
2020-05-20 16:27:08 -0600

    add comments

--- cypress/integration/login-spec.js
+++ cypress/integration/login-spec.js
@@ -8,14 +8,19 @@ describe('login test suite', () => {
         cy.get('[data-cy=password]').type('visitor');
         cy.get('[data-cy=login-form]').submit();
 
+        //find '.error-messages' class with child 'li' that contains specific message
         cy.contains('.error-messages li', 'email must be a valid email');
+
+        //Need to add assertiions that we are still in login page
     });
 
     it('happy path test', () => {
         cy.visit("http://localhost:4100");
         cy.get('[data-cy=sign-in]').click();
-        cy.get('[data-cy=username]').type('visitor');
+        cy.get('[data-cy=username]').type('infor@ad.com');
         cy.get('[data-cy=password]').type('visiting');
         cy.get('[data-cy=login-form]').submit();
+
+        //Need to add assertions that login happened correctly
     });
 })
