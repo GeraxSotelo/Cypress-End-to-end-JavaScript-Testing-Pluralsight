59f683f3b62588095d51482124a6e641bbb90adf
Gerax Sotelo <gsotelo23@gmail.com>
2020-05-26 16:03:05 -0600

    add test to publish article after using custom Cypress commands

--- cypress/integration/new-post-spec.js
+++ cypress/integration/new-post-spec.js
@@ -7,4 +7,18 @@ describe('New Post on Conduit', () => {
         cy.registerUserIfNeeded();
         cy.login();
     })
+
+    it('write a new post', () => {
+        //Fill details to write a new post
+        cy.get('[data-cy=new-post]').click();
+        cy.get('[data-cy=title]').type('My New Post');
+        cy.get('[data-cy=about]').type('This is my new post and I am excited');
+        cy.get('[data-cy=article]').type('Here is my lengthy article...');
+        //'enter' in curly braces uses the 'enter' key on the keyboard
+        cy.get('[data-cy=tags]').type('test{enter}');
+        cy.get('[data-cy=publish]').click();
+
+        //assertion to check url
+        cy.location('pathname').should('equal', '/article/my-new-post');
+    });
 })
