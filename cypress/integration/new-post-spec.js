5e3ee0efa098ad507fdc25e981b293aacc163fc6
Gerax Sotelo <gsotelo23@gmail.com>
2020-05-26 17:29:09 -0600

    write test to favorite an article

--- cypress/integration/new-post-spec.js
+++ cypress/integration/new-post-spec.js
@@ -40,8 +40,36 @@ describe('New Post on Conduit', () => {
         cy.location('pathname').should('equal', '/editor/my-new-post');
 
         cy.get('[data-cy=title]').clear().type('My Edited Title');
+        cy.get('[data-cy=article]').clear().type('Here is my edited lengthy article...');
         cy.get('[data-cy=tags]').clear().type('MyNewTag{enter}EditedArticle{enter}');
         cy.get('[data-cy=publish]').click();
         cy.location('pathname').should('equal', '/article/my-edited-title');
+    });
+
+    it('favorite an article test', () => {
+        //Fill details to write a new post
+        cy.get('[data-cy=new-post]').click();
+        cy.get('[data-cy=title]').type('My New Post');
+        cy.get('[data-cy=about]').type('This is my new post and I am excited');
+        cy.get('[data-cy=article]').type('Here is my lengthy article...');
+        cy.get('[data-cy=tags]').type('test{enter}');
+        cy.get('[data-cy=publish]').click();
+
+        //assertion to check url
+        cy.location('pathname').should('equal', '/article/my-new-post');
+
+        //go to profile page
+        cy.get('[data-cy=profile]').click();
+        cy.location('pathname').should('equal', '/@testuser');
+
+        //favorite the article
+        //find element with 'article-preview'. There should only be 1 for this test
+        //get the first 1, find the 'fav-article' button & click it.
+        cy.get('.article-preview').should('have.length', 1).first().find('[data-cy=fav-article]').click();
+
+        //validate that it is in the fav articles tab
+        cy.get('[data-cy=favorited-articles]').click();
+        cy.location('pathname').should('equal', '/@testuser/favorites');
+        cy.contains('.article-preview', 'My New Post');
     });
 })
