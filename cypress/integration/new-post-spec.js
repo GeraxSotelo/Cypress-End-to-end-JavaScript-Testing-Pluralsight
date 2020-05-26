f4c37ef2379ffa8f0e8b040015b1bf5856ed95e8
Gerax Sotelo <gsotelo23@gmail.com>
2020-05-26 16:55:42 -0600

    write test to edit an article

--- cypress/integration/new-post-spec.js
+++ cypress/integration/new-post-spec.js
@@ -21,4 +21,27 @@ describe('New Post on Conduit', () => {
         //assertion to check url
         cy.location('pathname').should('equal', '/article/my-new-post');
     });
+
+    it('edit article', () => {
+        //Fill details to write a new post
+        cy.get('[data-cy=new-post]').click();
+        cy.get('[data-cy=title]').type('My New Post');
+        cy.get('[data-cy=about]').type('This is my new post and I am excited');
+        cy.get('[data-cy=article]').type('Here is my lengthy article...');
+        //'enter' in curly braces uses the 'enter' key on the keyboard
+        cy.get('[data-cy=tags]').type('test{enter}');
+        cy.get('[data-cy=publish]').click();
+
+        //assertion to check url
+        cy.location('pathname').should('equal', '/article/my-new-post');
+
+        //editing the article
+        cy.get('[data-cy=edit-article]').click();
+        cy.location('pathname').should('equal', '/editor/my-new-post');
+
+        cy.get('[data-cy=title]').clear().type('My Edited Title');
+        cy.get('[data-cy=tags]').clear().type('MyNewTag{enter}EditedArticle{enter}');
+        cy.get('[data-cy=publish]').click();
+        cy.location('pathname').should('equal', '/article/my-edited-title');
+    });
 })
