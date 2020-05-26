cd87a8da50fec24a0d02d9a84190d5bcb995f067
Gerax Sotelo <gsotelo23@gmail.com>
2020-05-26 14:02:58 -0600

    add assertions to check for correct page navigation

--- cypress/integration/register-spec.js
+++ cypress/integration/register-spec.js
@@ -9,10 +9,18 @@ describe('Register', () => {
         cy.visit('http://localhost:4100');
         cy.contains('a.nav-link', 'Sign up').click();
 
+        //assertion. should navigate to registration page
+        cy.location('pathname').should('equal', '/register');
+
         cy.get('[data-cy=username]').type(username);
         cy.get('[data-cy=email]').type(email);
         cy.get('[data-cy=password]').type(password);
 
         cy.get('form').submit();
+
+        //should navigate to home page
+        cy.location('pathname').should('equal', '/');
+        //profile should be visible
+        cy.get('[data-cy=profile]').should('be.visible');
     });
 })
