9f2dbe471fe777b6f36f12f258c087e97898814a
Gerax Sotelo <gsotelo23@gmail.com>
2020-05-27 16:48:13 -0600

    write test to update profile settings

---
+++ cypress/integration/settings-spec.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+
+describe('Settings on Conduit', () => {
+    //adding a hook that runs before the test
+    beforeEach(() => {
+        cy.task('cleanDatabase');
+        cy.registerUserIfNeeded();
+        cy.login();
+    })
+
+    it('settings happy flow', () => {
+        cy.get('[data-cy=profile]').click();
+        cy.get('[data-cy=edit-profile-settings]').click();
+        cy.get('[data-cy=username]').clear().type('Updated User Name');
+        cy.get('[data-cy=bio]').clear().type('My new lengthy bio');
+        cy.get('form').submit();
+    });
+})
